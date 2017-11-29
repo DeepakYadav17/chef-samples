74b550b7cfa1192df089ea2f07fc021a362de6c3
root <root@ip-172-31-28-179.us-east-2.compute.internal>
2017-11-29 06:34:17 +0000

    adding chef recipes for tomcat and mysql installation

---
+++ cookbooks/tomcat_and_mysql/metadata.rb
@@ -0,0 +1,20 @@
+name 'tomcat_and_mysql'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'All Rights Reserved'
+description 'Installs/Configures Tomcat and MySQL'
+long_description 'Installs/Configures tomcat_and_mysql'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+#
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+#
---
+++ cookbooks/tomcat_and_mysql/recipes/default.rb
@@ -0,0 +1,58 @@
+#
+# Cookbook:: learn_chef_httpd
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+#package 'httpd'
+
+#service 'httpd' do
+#  action [:enable, :start]
+#end
+
+#template '/var/www/html/index.html' do
+#  source 'index.html.erb'
+#end
+
+#execute 'java install' do
+#  command 'yum install java -y'
+#end
+
+# Install Tomcat 8.0.47 to the default location
+remote_file '/opt/apache-tomcat-8.0.47.tar.gz' do
+  source 'http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.47/bin/apache-tomcat-8.0.47.tar.gz'
+  mode '0755'
+  action :create
+end
+
+
+execute 'extract tomcat' do
+  cwd '/opt'
+  command 'tar -xvf apache-tomcat-8.0.47.tar.gz'
+end
+
+remote_file '/opt/apache-tomcat-8.0.47/webapps/sample.war' do
+  mode '0644'
+  source 'https://tomcat.apache.org/tomcat-6.0-doc/appdev/sample/sample.war'
+  checksum '89b33caa5bf4cfd235f060c396cb1a5acb2734a1366db325676f48c5f5ed92e5'
+end
+
+execute 'install mysql server' do
+  command 'yum install -y mysql-server'
+end
+
+execute 'install mysql client' do
+  command 'yum install -y mysql'
+end
+  
+
+
+
+
+
+
+#tomcat_install 'helloworld' do
+#  tarball_uri 'http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.47/bin/apache-tomcat-8.0.47.tar.gz'
+#  tomcat_user 'cool_user'
+#  tomcat_group 'cool_group'
+#end
